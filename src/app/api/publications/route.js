fd27e684aa5815aceb5bdcb3a14ea745925d0b96
Haseeb Ahmed <haseeb@spectreco.com>
2025-02-25 12:35:06 +0500

    OPTIONS ADDED

--- src/app/api/publications/route.js
+++ src/app/api/publications/route.js
@@ -40,7 +40,6 @@ export async function POST(request) {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type",
       },
     });
@@ -54,4 +53,15 @@ export async function POST(request) {
       },
     });
   }
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
